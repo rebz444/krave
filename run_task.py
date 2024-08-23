20054e43a70469a1b2f08b5e9bf0cf272792483f
Rebekah Zhang <rebz.444@gmail.com>
2024-08-23 12:02:57 -0400

    changed print statement for session ending.

--- run_task.py
+++ run_task.py
@@ -2,7 +2,7 @@ from krave.experiment.task import Task
 
 if __name__ == '__main__':
     Task(mouse="test",
-         rig_name="rig1",
+         rig_name="rig7",
          training="regular",
          trainer="Rebekah",
          record=True).run()
